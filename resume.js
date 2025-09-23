2b2ef0a0285b25c4bedbba8505b2f6e92e46f6b9
makanit21-netizen <makanit21@gmail.com>
2025-09-23 14:50:15 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -9,13 +9,25 @@ let myEmail = "mtanaka12@oru.edu";
 let title = "Student Athlete";
 let education = "Oral Roberts University";
 let experience = "Baseball, Business Student, and Politics";
-let skills  = []
+let skills  = ["Baseball", "Leadership", "Communication", "Teamwork"];
+
 
 // Connect Variables to the HTML (ligaments)
 
 document.getElementById("my-name").textContent = myName;
 document.getElementById("my-email").textContent = myEmail;
 document.getElementById("title").textContent = title;
-document.getElementById("Experience").textContent = experience;
-document.getElementById("Education").textContent = education;
-document.getElementById("Skills").textContent = skills;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
+document.getElementById("skills").textContent = skills;
+
+for (let i = 0; i < skills.length; i++) {
+ let li = document.createElement("li");
+ li.textContent = skills[i];
+ skillsList.appendChild(li);
+}
+document.body.appendChild(skillsList);
+
+function showFunFact() {
+ alert("Fun Fact: I love my pet pig!");
+}
